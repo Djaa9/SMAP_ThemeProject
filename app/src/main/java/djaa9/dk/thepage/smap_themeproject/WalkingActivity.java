5ba3e34307b22b809cc9c387be8b010869a3102f
Johnny Poulsen <jp@thepage.dk>
2015-05-16 14:56:51 +0200

    Polyline drawing implemented

--- app/src/main/java/djaa9/dk/thepage/smap_themeproject/WalkingActivity.java
+++ app/src/main/java/djaa9/dk/thepage/smap_themeproject/WalkingActivity.java
@@ -3,6 +3,7 @@ package djaa9.dk.thepage.smap_themeproject;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -13,13 +14,21 @@ import android.view.View;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.maps.model.internal.IPolylineDelegate;
+
+import java.util.List;
 
 
 public class WalkingActivity extends FragmentActivity implements LocationListener, OnMapReadyCallback{
     private LocationManager _locationManager;
     private GoogleMap _map;
+    private List<LatLng> _route;
+    private PolylineOptions _polylineOptions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +36,17 @@ public class WalkingActivity extends FragmentActivity implements LocationListene
         setContentView(R.layout.activity_walking);
 
         ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMapAsync(this);
+
         _locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
         _locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
+        //_route = new List<>();
 
-        //_locationManager.s
+         _polylineOptions = new PolylineOptions()
+                .color(Color.RED)
+                .geodesic(true)
+                .visible(true)
+                .width(10);
 
     }
 
@@ -44,12 +59,11 @@ public class WalkingActivity extends FragmentActivity implements LocationListene
 
     @Override
     public void onLocationChanged(Location location) {
+        //_route.add(new LatLng(location.getLatitude(), location.getLongitude()));
 
+        _polylineOptions.add(new LatLng(location.getLatitude(), location.getLongitude()));
 
-        _map.addMarker(new MarkerOptions()
-                .title("")
-                .snippet("")
-                .position(new LatLng(location.getLatitude(), location.getLongitude())));
+        _map.addPolyline(_polylineOptions);
     }
 
     @Override
@@ -69,6 +83,9 @@ public class WalkingActivity extends FragmentActivity implements LocationListene
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-    _map = googleMap;
+        _map = googleMap;
+        _map.setMyLocationEnabled(true);
+
+        _map.addPolyline(_polylineOptions);
     }
 }
